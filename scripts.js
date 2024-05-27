7c71e8e384fbd7ad7286a9169c86c735d11b1ac5
Guilherme Muniz <146376983+Guimunizzs@users.noreply.github.com>
2024-05-27 16:56:17 -0300

    js-finalizado

--- scripts.js
+++ scripts.js
@@ -5,8 +5,35 @@ let items = container.querySelectorAll(".list .item");
 let indicator = document.querySelector(".indicators");
 let dots = indicator.querySelectorAll("ul li");
 
-nextButton.onclick = () => {};
+let active = 0
+let firstPosition = 0
+let lastPosition = items.length - 1
+
+function setSlider() {
+  let itemOld = container.querySelector('.list .item.active')
+  itemOld.classList.remove('active')
+
+  let dotsOld = indicator.querySelector('ul li.active')
+  dotsOld.classList.remove('active')
+  dots[active].classList.add('active')
+
+  indicator.querySelector('.number').innerHTML = "0" + (active + 1)
+}
+
+nextButton.onclick = () => {
+
+  active = active + 1 > lastPosition ? 0 : active + 1
+  setSlider()
+  items[active].classList.add('active')
+
+
+
+};
 
 prevButton.onclick = () => {
-  console.log("Botao return");
+  active = active - 1 < firstPosition ? lastPosition : active - 1
+  setSlider()
+  items[active].classList.add('active')
+
+
 };
